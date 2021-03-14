fcbc79971d6400022d32dc062494498206c7bb52
Aaron-Helfrich <72519582+Aaron-Helfrich@users.noreply.github.com>
2021-03-14 11:28:50 -0400

    Add files via upload

---
+++ DiceSim.java
@@ -0,0 +1,39 @@
+import java.util.*; 
+
+public class DiceSim{
+
+
+    static Scanner sc = new Scanner(system.in);
+  
+    public static void main(String args[]) 
+    
+    { 
+        // create instance of Random class 
+        Random rand = new Random(); 
+
+         // Generate random integers in range 1 to 6
+	  int rand_array[];
+	  int N = sc.nextInt(); 
+	  rand_array = new int[N];
+      for (int i = 0; i < N; i++); 
+        {
+            rand_array[i] = rand.nextInt{6);
+                System.out.println("dice " + i+1 + " is " + rand_array[i]);
+        }
+        
+         
+            //int rand_int1 = rand.nextInt(6); 
+        
+        //int out = rand_int1+1;
+        // Print random integer 
+        //System.out.println("your roll is "+out);
+    }
+}
+
+
+	  
+	
+ 
+ 
+
+       
