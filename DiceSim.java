4ab5af99b5eb58e19a562526aded20133747605c
Aaron-Helfrich <72519582+Aaron-Helfrich@users.noreply.github.com>
2021-04-15 11:44:29 -0400

    Add files via upload
    Final version of dice sim

--- DiceSim.java
+++ DiceSim.java
@@ -11,20 +11,34 @@ public class DiceSim{
         // create instance of Random class 
         Random rand = new Random(); 
 
-         // Generate random integers in range 1 to 6
-        int type = sc.nextInt();
-	  int rand_array[];
-	  int TimesToRoll = sc.nextInt(); 
-	  rand_array = new int[TimesToRoll];
-      for (int i = 0; i <= type; i++) 
-        
-      {
-            rand_array[i] = rand.nextInt(TimesToRoll);
-                System.out.println("dice " + (i+1) + " is " + rand_array[i]);
+        // Generate random integers in range 1 to 6
+        System.out.println("Enter the number of sides for your dice");
+        int Type = sc.nextInt();
+        int rand_array[];
+        System.out.println("How many dice do you want to roll");
+	    int TimesToRoll = sc.nextInt();
+	        while (true)
+	    {
+	        System.out.println("Do you want to roll dice? (1 = yes, 0 = no) ");
+	        int yn = sc.nextInt();
+                if (yn == 1)
+            {	
+                int total = 0; 
+	        rand_array = new int[TimesToRoll];
+            for (int i = 0; i < TimesToRoll; i++) 
+            {
+            rand_array[i] = 1 + rand.nextInt(Type);
+	        total += rand_array[i];
+            //System.out.println("dice " + (i+1) + " is " + rand_array[i]);
         }
-        
-         
-            //int rand_int1 = rand.nextInt(6); 
+            System.out.println("total of all dice is " + total);
+        }
+	else
+{
+	break;
+}
+	}
+        //int rand_int1 = rand.nextInt(6); 
         
         //int out = rand_int1+1;
         // Print random integer 
